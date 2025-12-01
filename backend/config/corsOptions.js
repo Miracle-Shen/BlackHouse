403a9ba91591cabb82dc604edd4acd43a04f1057
Miralce-Shn <miracle.qishen@gmail.com>
2025-12-01 21:08:35 +0800

    version:init

--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -1,5 +1,5 @@
 const whitelist = [
-    'https://www.yoursite.com',
+    'https://black-house-frontend.vercel.app',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:5173'
