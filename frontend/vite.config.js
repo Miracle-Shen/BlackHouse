c68fcf16ce8b38c4b713c363711968278262226e
Miralce-Shn <miracle.qishen@gmail.com>
2025-12-09 19:24:09 +0800

    feat:vite vendor divide

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import checker from 'vite-plugin-checker';
 import { visualizer } from "rollup-plugin-visualizer";
+import { fa } from 'zod/v4/locales';
 export default defineConfig({
     plugins: [
         react(), checker({ typescript: true }),
@@ -17,5 +18,18 @@ export default defineConfig({
         alias: {
             '@': path.resolve(__dirname, './src'), // 将 @ 映射到 src 目录
         },
+    },
+    build: {
+        sourcemap: false,
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                },
+            },
+        },
+
     },
 });
